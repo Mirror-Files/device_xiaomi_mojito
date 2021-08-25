d2706e516e1fb9258fcd7806504842f1488e9439
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-08-25 17:29:28 +0530

    Update and rename spark_mojito.mk to potato_mojito.mk

--- potato_mojito.mk
+++ potato_mojito.mk
@@ -15,10 +15,10 @@ $(call inherit-product, device/xiaomi/mojito/device.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_mojito
+PRODUCT_NAME := potato_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
